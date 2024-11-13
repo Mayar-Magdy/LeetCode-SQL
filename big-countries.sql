c659cbb692564e09ab312773d035351589ee9221
Mayar-Magdy <maimagdy238@gmail.com>
2024-11-13 17:05:05 +0200

    Solved problem: Big Countries

---
+++ big-countries.sql
@@ -0,0 +1,13 @@
+-- Problem link
+-- https://leetcode.com/problems/big-countries/description/
+
+-- Write your MySQL query statement below
+SELECT 
+    name, 
+    population, 
+    area 
+FROM 
+    World 
+WHERE 
+    area >= 3000000 
+    OR population >= 25000000;
